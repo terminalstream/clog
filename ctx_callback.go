ae8a9770b68c922b7c728302191e67b2e1cdba0b
George Aristy <garisty@terminalstream.tech>
2025-02-18 21:21:59 -0500

    test

--- ctx_callback.go
+++ ctx_callback.go
@@ -16,17 +16,16 @@ func (c *entryFieldCallbacks) Check(entry zapcore.Entry, checked *zapcore.Checke
 }
 
 func (c *entryFieldCallbacks) Write(entry zapcore.Entry, fields []zapcore.Field) error {
-	err := c.Core.Write(entry, fields)
-
-	for _, cb := range c.cbs {
-		cb(entry, fields)
+	for i := range c.cbs {
+		c.cbs[i](entry, fields)
 	}
 
-	return err
+	return c.Core.Write(entry, fields)
 }
 
 func (c *entryFieldCallbacks) With(fields []zapcore.Field) zapcore.Core {
-	c.Core = c.Core.With(fields)
-
-	return c
+	return &entryFieldCallbacks{
+		Core: c.Core.With(fields),
+		cbs:  c.cbs,
+	}
 }
