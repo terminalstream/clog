3b5790c436109633cbe4bae0978e9db3d27d6439
George Aristy <garisty@terminalstream.tech>
2025-02-18 21:44:45 -0500

    test

--- ctx_callback.go
+++ ctx_callback.go
@@ -4,7 +4,8 @@ import "go.uber.org/zap/zapcore"
 
 type entryFieldCallbacks struct {
 	zapcore.Core
-	cbs []func(zapcore.Entry, []zapcore.Field)
+	cbs     []func(zapcore.Entry, []zapcore.Field)
+	context []zapcore.Field
 }
 
 func (c *entryFieldCallbacks) Check(entry zapcore.Entry, checked *zapcore.CheckedEntry) *zapcore.CheckedEntry {
@@ -16,8 +17,10 @@ func (c *entryFieldCallbacks) Check(entry zapcore.Entry, checked *zapcore.Checke
 }
 
 func (c *entryFieldCallbacks) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	allFields := append(c.context, fields...)
+
 	for i := range c.cbs {
-		c.cbs[i](entry, fields)
+		c.cbs[i](entry, allFields)
 	}
 
 	return c.Core.Write(entry, fields)
@@ -25,7 +28,8 @@ func (c *entryFieldCallbacks) Write(entry zapcore.Entry, fields []zapcore.Field)
 
 func (c *entryFieldCallbacks) With(fields []zapcore.Field) zapcore.Core {
 	return &entryFieldCallbacks{
-		Core: c.Core.With(fields),
-		cbs:  c.cbs,
+		Core:    c.Core.With(fields),
+		cbs:     c.cbs,
+		context: append(c.context, fields...),
 	}
 }
